a3479f2eef097f956871752a457e7ae1d9197209
Pedro Sequeira <pmcsequeira@gmail.com>
2023-03-08 16:16:22 +0000

    Added functions for state changes

--- app/src/main/java/com/pedrosequeira/movies/movielist/models/MovieListState.kt
+++ app/src/main/java/com/pedrosequeira/movies/movielist/models/MovieListState.kt
@@ -3,4 +3,15 @@ package com.pedrosequeira.movies.movielist.models
 internal data class MovieListState(
     val isLoading: Boolean = true,
     val movies: List<String>? = null
-)
+) {
+    fun loading(): MovieListState {
+        return this.copy(isLoading = true)
+    }
+
+    fun movies(movies: List<String>): MovieListState {
+        return this.copy(
+            isLoading = false,
+            movies = movies
+        )
+    }
+}
--- app/src/main/java/com/pedrosequeira/movies/movielist/presentation/MovieListViewModel.kt
+++ app/src/main/java/com/pedrosequeira/movies/movielist/presentation/MovieListViewModel.kt
@@ -1,27 +1,29 @@
 package com.pedrosequeira.movies.movielist.presentation
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.pedrosequeira.movies.movielist.models.MovieListState
 import com.pedrosequeira.movies.movielist.repository.MoviesRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 internal class MovieListViewModel(
     private val repository: MoviesRepository
-) {
+) : ViewModel() {
 
     private val _state = MutableStateFlow(MovieListState())
     val state: StateFlow<MovieListState> = _state.asStateFlow()
 
     fun fetchMovies() {
-        _state.update { it.copy(isLoading = true) }
-        val movies = repository.fetchMovies()
-        _state.update {
-            it.copy(
-                isLoading = false,
-                movies = movies
-            )
+        viewModelScope.launch {
+            _state.update { it.loading() }
+            val movies = repository.fetchMovies()
+            _state.update {
+                it.movies(movies)
+            }
         }
     }
 }
